1521433d7d83bc1e7c6b972ed662030eb4fe22b8
AddeusExMachina <antaddnf@gmail.com>
2023-12-27 19:46:01 +0100

    Add channels in server.c

--- server.c
+++ server.c
@@ -26,36 +26,55 @@
 #include "socketlib.h"
 
 #define MAX_CLIENTS 1000
+#define MAX_CHANNELS 100
 #define PORT 50001
 
-/* For each client we keep information about the username, the position in the file descriptor set
- * and pointers to next and previous clients. Clients are managed in a double linked list fashion
- * to easily iterate among them. */
+/* For each client we keep information about the username, the position in the file descriptor set and the cannel.
+ * Moreover there are pointers to next and previous clients in the chat and in the same channel. */
 struct Client {
 	char* username;
 	int fdsIndex;
-	struct Client* next;
-	struct Client* prev;
+	struct Client* nextInChat;
+	struct Client* prevInChat;
+	struct Client* nextInChannel;
+	struct Client* prevInChannel;
+	struct Channel* channel;
 };
-struct Client* head;
-struct Client* tail;
+struct Client* chatHead;
+struct Client* chatTail;
 
 /* A container from which a given client can be found: the key is actually the
  * client's username. */
-struct bucket {
+struct ClientBucket {
 	char* key;
 	struct Client* value;
-	struct bucket* next;
+	struct ClientBucket* nextInChat;
 };
-struct bucket *hashtable[MAX_CLIENTS];
+struct ClientBucket *clientHashtable[MAX_CLIENTS];
+
+struct Channel {
+	char *name;
+	struct Channel *nextInChat;
+	struct Client* head;
+	struct Client* tail;
+};
+
+/* A container from which a given channel can be found: the key is actually the
+ * channel's name. */
+struct ChannelBucket {
+	char* key;
+	struct Channel* value;
+	struct ChannelBucket* nextInChat;
+};
+struct ChannelBucket *channelHashtable[MAX_CHANNELS];
 
 /* Simple hash evaluation for a string as in section 6.6 of 'The C Programming Language' */
-int hash(char* s) {
+int hash(char* s, int size) {
 	int hashValue;
 	for (hashValue = 0; *s != '\0'; s++) {
-		hashValue = *s + 31 * hashValue;
+		hashValue = *s + 31 * hashValue % size;
 	}
-	return hashValue % MAX_CLIENTS;
+	return hashValue;
 }
 
 /* Remove a client's bucket from the hashtable collection by the key (client's username).
@@ -63,40 +82,71 @@ int hash(char* s) {
  * advantage of using the undirect pointer b to avoid handling the special case
  * for removing the head. */
 void deleteClientByUsername(char* username) {
-	struct bucket **b = &hashtable[hash(username)];
+	struct ClientBucket **b = &clientHashtable[hash(username, MAX_CLIENTS)];
 	while (*b != NULL && strcmp((*b)->key, username) != 0) {
-		b = &(*b)->next;
+		b = &(*b)->nextInChat;
 	}
 	if (*b != NULL) {
-		*b = (*b)->next;
+		*b = (*b)->nextInChat;
 	}
 }
 
-/* Insert a pair username-client in hashtable.
+/* Insert a pair username-client in clientHashtable.
  * There is the same reasoning for the undirect pointer b as in deleteClientByUsername. */
 void insertClient(char* username, struct Client* c) {
-	struct bucket **b = &hashtable[hash(username)];
+	struct ClientBucket **b = &clientHashtable[hash(username, MAX_CLIENTS)];
 	while (*b != NULL) {
-		b = &(*b)->next;
+		b = &(*b)->nextInChat;
 	}
 
-	struct bucket* newBucket;
+	struct ClientBucket* newBucket;
 	newBucket = malloc(sizeof(newBucket));
 	newBucket->key = strdup(username);
 	newBucket->value = c;
 	*b = newBucket;
 }
 
+/* Insert a pair name-channel in channelHashtable.
+ * There is the same reasoning for the undirect pointer b as in deleteClientByUsername. */
+void insertChannel(char* name, struct Channel* c) {
+	struct ChannelBucket **b = &channelHashtable[hash(name, MAX_CHANNELS)];
+	while (*b != NULL) {
+		b = &(*b)->nextInChat;
+	}
+
+	struct ChannelBucket* newBucket;
+	newBucket = malloc(sizeof(newBucket));
+	newBucket->key = strdup(name);
+	newBucket->value = c;
+	*b = newBucket;
+}
+
 /* Find the client, if present, contained in the bucket whose key is username. */
 struct Client* getClientByUsername(char* username) {
-	int hashValue = hash(username);
-	struct bucket* curr = hashtable[hashValue];
+	int hashValue = hash(username, MAX_CLIENTS);
+	struct ClientBucket* curr = clientHashtable[hashValue];
 	while (curr != NULL) {
 		if (strcmp(curr->key, username) == 0) {
 			/* Client found */
 			return curr->value;
 		} else {
-			curr = curr->next;
+			curr = curr->nextInChat;
+		}
+	}
+	/* No client with that username has been found. */
+	return NULL;
+}
+
+/* Find the channel, if present, contained in the bucket whose key is name. */
+struct Channel* getChannelByName(char* name) {
+	int hashValue = hash(name, MAX_CHANNELS);
+	struct ChannelBucket* curr = channelHashtable[hashValue];
+	while (curr != NULL) {
+		if (strcmp(curr->key, name) == 0) {
+			/* Channel found */
+			return curr->value;
+		} else {
+			curr = curr->nextInChat;
 		}
 	}
 	/* No client with that username has been found. */
@@ -110,51 +160,79 @@ struct pollfd fds[MAX_CLIENTS + 1];
 int numClients = 0;
 
 /* Discard all info about a client by releasing and overwriting the related resources.
- * As side effect we update the fds entries and if necessary also head and tail. */
-void freeClient(struct Client *client) {
-	/* Close that client's file descriptor. */
+ * As side effect we update the fds entries and if necessary also head and tail for the chat and the channel. */
+void freeClient(struct Client* client) {
 	close(fds[client->fdsIndex].fd);
-	/* Deallocate the space for username string. */
 	free(client->username);
 
-	/* Tail client will occupy the entry in fds where the currently deleting client was at. 
-	 * To do it we overwrite the client data in its fds entry with the tail data and invalidate the
-	 * data present at tail index. */
-	fds[client->fdsIndex].fd = fds[tail->fdsIndex].fd;
-	fds[tail->fdsIndex].fd = -1;
-	fds[tail->fdsIndex].events = 0;
-	int revents = fds[tail->fdsIndex].revents;
-	fds[tail->fdsIndex].revents = 0;
-	tail->fdsIndex = client->fdsIndex;
-	/* Ignore the returned events of tail only if it is the client removed, 
-	 * otherwise restore that info for tail. */
-	fds[tail->fdsIndex].revents = tail == client ? 0 : revents;
+	/* chatTail client will occupy the entry in fds where the currently deleting client was at. 
+	 * To do it we overwrite the client data in its fds entry with the chatTail data and invalidate the
+	 * data present at chatTail index. */
+	fds[client->fdsIndex].fd = fds[chatTail->fdsIndex].fd;
+	fds[chatTail->fdsIndex].fd = -1;
+	fds[chatTail->fdsIndex].events = 0;
+	int revents = fds[chatTail->fdsIndex].revents;
+	fds[chatTail->fdsIndex].revents = 0;
+	chatTail->fdsIndex = client->fdsIndex;
+	/* Ignore the returned events of chatTail only if it is the client removed, 
+	 * otherwise restore that info for chatTail. */
+	fds[chatTail->fdsIndex].revents = chatTail == client ? 0 : revents;
 
 	/* The client that might be the new tail. */
-	struct Client *prevTail = tail->prev;
+	struct Client *prevTail = chatTail->prevInChat;
 
-	/* Update tail next pointer and client next prev paying attention if: 
-	 * 1. we remove the tail (NULL reference)
-	 * 2. we remove the client before tail (we may create a loop) */
-	if (client != tail && client != prevTail) {
-		tail->next = client->next;
-		client->next->prev = tail;
+	/* Update chatTail next pointer and client next prev paying attention if: 
+	 * 1. we remove chatTail (NULL reference)
+	 * 2. we remove the client before chatTail (we may create a loop) */
+	if (client != chatTail && client != prevTail) {
+		chatTail->nextInChat = client->nextInChat;
+		client->nextInChat->prevInChat = chatTail;
 	}
-	tail->prev = client->prev;
-	if (client == head) {
-		/* If head is being removed update head with tail only if there is at least another client. */
-		head = head == tail ? NULL : tail;
+	chatTail->prevInChat = client->prevInChat;
+	if (client == chatHead) {
+		/* If chatHead is being removed update chatHead with chatTail only if there is at least another client. */
+		chatHead = chatHead == chatTail ? NULL : chatTail;
 	} else {
 		/* For any other client just update the next pointer of its previous. */
-		client->prev->next = tail;
+		client->prevInChat->nextInChat = chatTail;
 	}
-	/* We update tail to prevTail only if we're not deleting prevTail, otherwise tail is unchanged. */
+	/* We update chatTail to prevTail only if we're not deleting prevTail, otherwise chatTail is unchanged. */
 	if (prevTail != client) {
-		tail = prevTail;
-		if (tail != NULL) {
-			tail->next = NULL;
+		chatTail = prevTail;
+		if (chatTail != NULL) {
+			chatTail->nextInChat = NULL;
 		}
 	}
+
+	struct Channel* channel = client->channel;
+	struct Client* channelHead = channel->head;
+	struct Client* channelTail = channel->tail;
+	/* The client that might be the new channel tail. */
+	prevTail = channelTail->prevInChannel;
+
+	/* Update channelTail next pointer and client next prev paying attention if: 
+	 * 1. we remove channelTail (NULL reference)
+	 * 2. we remove the client before channelTail (we may create a loop) */
+	if (client != channelTail && client != prevTail) {
+		channelTail->nextInChannel = client->nextInChannel;
+		client->nextInChannel->prevInChannel = channelTail;
+	}
+	channelTail->prevInChannel = client->prevInChannel;
+	if (client == channelHead) {
+		/* If channel head is being removed update channel head with channelTail only if there is at least another client. */
+		channel->head = channelHead == channelTail ? NULL : channelTail;
+	} else {
+		/* For any other client just update the next pointer of its previous. */
+		client->prevInChannel->nextInChannel = channelTail;
+	}
+	/* We update channelTail to prevTail only if we're not deleting prevTail, otherwise tail is unchanged. */
+	if (prevTail != client) {
+		channel->tail = prevTail;
+		if (channelTail != NULL) {
+			channelTail->nextInChannel = NULL;
+		}
+	}
+
 }
 
 /* In main() first we create the server socket, then
@@ -197,16 +275,17 @@ int main() {
 				client->fdsIndex = numClients + 1;
 				fds[0].events = POLLIN;
 
-				if (head == NULL) {
+				if (chatHead == NULL) {
 					/* First client inserted. */
-					head = client;
+					chatHead = client;
 				} else {
-					/* Append the client to tail. */
-					tail->next = client;
-					client->prev = tail;
+					/* Append the client to chatTail. */
+					chatTail->nextInChat = client;
+					client->prevInChat = chatTail;
 				}
-				tail = client;
+				chatTail = client;
 
+				/* Update clientHashtable. */
 				insertClient(username, client);
 
 				fds[client->fdsIndex].fd = clientFD;
@@ -217,7 +296,7 @@ int main() {
 				send(clientFD, welcomeMessage, strlen(welcomeMessage), 0);
 			}
 
-			for (struct Client *client = head; client != NULL; client = client->next) {
+			for (struct Client *client = chatHead; client != NULL; client = client->nextInChat) {
 				int fdsIndex = client->fdsIndex;
 
 				if (fds[fdsIndex].revents & POLLIN) {
@@ -242,6 +321,8 @@ int main() {
 								memcpy(newUsername, buffer + 13, newUsernameLength);
 								newUsername[newUsernameLength - 1] = '\0';
 
+								/* If the username already exists we ignore the command,
+								 * otherwise we update the client's username in clientHashtable. */
 								if (getClientByUsername(newUsername) != NULL) {
 									send(fds[fdsIndex].fd, "Username already exists\n", 24, 0);
 									memset(buffer, 0, sizeof buffer);
@@ -255,13 +336,44 @@ int main() {
 								/* The user closed the connection */
 								deleteClientByUsername(client->username);
 								freeClient(client);
+							} else if (strncmp(buffer+1, "join", 4) == 0) {
+								/* The user wants to join a channel, the channel's name
+								 * is the string after '\join ' whose length is 6. */
+								int length = bytesRead - 6;
+								char* name = malloc(length);
+								memcpy(name, buffer + 6, length);
+								name[length - 1] = '\0';
+
+								/* Create the channel if it doesn't exist. */
+								struct Channel* channel;
+								if ((channel = getChannelByName(name)) == NULL) {
+									channel = malloc(sizeof(channel));
+									channel->name = name;
+									insertChannel(name, channel);
+								}
+
+								/* Update the channel state appending the current client. */
+								client->channel = channel;
+								if (channel->head == NULL) {
+									channel->head = client;
+								} else {
+									channel->tail->nextInChannel = client;
+									client->prevInChannel = channel->tail;
+								}
+								channel->tail = client;
 							}
 						} else {
-							/* If the client sent a message, broadcast the message */
+							/* The client sent a message, broadcast the message */
+							if (client->channel == NULL) {
+								/* Ignore the message if the client is in none channel. */
+								continue;
+							}
+
+							/* Otherwise broadcast the message in that channel. */
 							char message[1024];
 							int messageLength = snprintf(message, sizeof(message), "%s> %s", client->username, buffer);
 
-							for (struct Client *c = head; c != NULL; c = c->next) {
+							for (struct Client *c = client->channel->head; c != NULL; c = c->nextInChannel) {
 								if (c != client) {
 									send(fds[c->fdsIndex].fd, message, messageLength, 0);
 								}
